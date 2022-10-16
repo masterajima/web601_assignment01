0a517b6cfbd4f950a9213bc9f6ea14831458d2e5
Ajima Master <98704309+masterajima@users.noreply.github.com>
2022-10-16 17:16:45 -0400

    changing color based on state

--- WEB601/App.js
+++ WEB601/App.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import React from "react";
 const App = () => {
-   const [tempratureValue, setTempratureValue] = useState(10);
+  const [tempratureValue, setTempratureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+  const increaseTemperature = () => {
+    setTempratureValue(tempratureValue + 1);
+    setTempratureValue(newTemperature);
+    if (newTemperature >= 15) {
+      setTemperatureColor("hot");
+    }
+  };
+  const decreaseTemperature = () => {
+    setTemperatureValue(tempratureValue - 1);
+    setTemperatureValue(newTemperature);
+    if (newTemperature < 15) {
+      setTemperatureColor("cold");
+    }
+  };
+
   return (
     <div className="app-container">
-      <div className="emperature-display-container">
-        <div className='temperature-display'>10°C</div>
-        <div className="button-container">
-          <button <button onClick={() => setTempratureValue(tempratureValue + 1)}>
-          >+</button>
-          <button onClick={() => setTempratureValue(tempratureValue - 1)}>-</button>
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temperatureColor}`}>
+          {tempratureValue}°C
         </div>
+      </div>
+      <div className="temperature-display">{tempratureValue}°C</div>
+      <div className="button-container">
+        <button></button>
+        <button onClick={() => setTempratureValue(tempratureValue + 1)}>
+          +
+        </button>
+        <button onClick={() => setTempratureValue(tempratureValue - 1)}>
+          -
+        </button>
       </div>
     </div>
   );
